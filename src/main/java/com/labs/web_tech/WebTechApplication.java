c7e5d8686747eaeb185ddd842a6c81c2efb3d212
Sviatoslav <sviatoslav.kytsara@artcodevision.com>
2022-10-15 16:12:12 +0300

    first commit

---
+++ src/main/java/com/labs/web_tech/WebTechApplication.java
@@ -0,0 +1,13 @@
+package com.labs.web_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labs/web_tech/WebTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.web_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
